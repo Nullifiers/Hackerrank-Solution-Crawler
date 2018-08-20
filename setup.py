3e72512d8ab951d0d4c7851f4bbceb3f36d330d4
rajat <rajatsri94@gmail.com>
2018-08-20 17:02:35 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name='hackerrank-crawler',
+	version='1.1.0',
+	author='Nullifiers',
+	description='hackerrank solution crawler',
+	url='https://github.com/Nullifiers/Hackerrank-Solution-Crawler',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
