066a0b0e45a151c04b5e705e868eb4e35aab22c6
Rajat Goyal <rajatgoyal715@gmail.com>
2018-09-21 01:00:58 +0530

    Add entry points in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
 	name='hackerrank-crawler',
 	version='1.1.0',
 	author='Nullifiers',
-	description='hackerrank solution crawler',
+	description='Hackerrank Solution Crawler',
 	url='https://github.com/Nullifiers/Hackerrank-Solution-Crawler',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
@@ -17,4 +17,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+	entry_points='''
+		[console_scripts]
+		hackerrank-crawler=hackerrank.crawler:main
+	''',
 )
