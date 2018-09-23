5b9e538caabda78c9f9938b53bff225d0e93fa01
Rajat Goyal <rajatgoyal715@gmail.com>
2018-09-23 21:56:27 +0530

    Update setup script to include entry point

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup (
 	name='hackerrank-crawler',
-	version='1.1.0',
+	version='1.0.1',
 	author='Nullifiers',
+	author_email='nullifiersorg@gmail.com',
 	description='Hackerrank Solution Crawler',
 	url='https://github.com/Nullifiers/Hackerrank-Solution-Crawler',
+	download_url='https://github.com/Nullifiers/Hackerrank-Solution-Crawler/releases',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	packages=setuptools.find_packages(),
+	packages=find_packages(),
 	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-	entry_points='''
-		[console_scripts]
-		hackerrank-crawler=hackerrank.crawler:main
-	''',
+	entry_points={
+		'console_scripts': [
+			'hackerrank-crawler=hackerrank-crawler.crawler:main',
+		],
+	}
 )
