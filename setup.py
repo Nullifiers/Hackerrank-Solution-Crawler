985813bbce3c9398529477f50665d94bcbb0c90e
Rajat Goyal <rajatgoyal715@gmail.com>
2019-10-06 16:34:55 +0530

    Update version in setup script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup (
 	name='hsc',
-	version='1.0.2',
+	version='1.1.1',
 	author='Nullifiers',
 	author_email='nullifiersorg@gmail.com',
 	description='Hackerrank Solution Crawler',
