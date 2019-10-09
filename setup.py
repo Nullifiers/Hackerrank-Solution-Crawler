d58d3e375fa61daadab1c4ecac44d00b990252cf
Rajat Srivastava <rajatsri94@gmail.com>
2019-10-10 00:46:10 +0530

    Update version to 1.2 as major update in usage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hsc',
-    version='1.1.3',
+    version='1.2.0',
     author='Nullifiers',
     author_email='nullifiersorg@gmail.com',
     description='Hackerrank Solution Crawler',
