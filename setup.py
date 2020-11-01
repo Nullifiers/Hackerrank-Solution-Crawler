35791e02f849bbe8bf8fa413eb558f8388dbddf2
Rajat Goyal <rajatgoyal715@gmail.com>
2020-11-01 16:06:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hsc',
-    version='1.2.3-beta',
+    version='1.2.4-alpha',
     author='Nullifiers',
     author_email='nullifiersorg@gmail.com',
     description='Hackerrank Solution Crawler',
